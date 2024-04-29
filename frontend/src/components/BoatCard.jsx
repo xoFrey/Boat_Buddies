642d1fe01cde6c199f3649cb71914c5a11e8e84d
xofrey <xizelacar@gmail.com>
2024-04-29 15:46:13 +0200

    All boats

--- frontend/src/components/BoatCard.jsx
+++ frontend/src/components/BoatCard.jsx
@@ -1,5 +1,14 @@
-const BoatCard = () => {
-  return <div>BoatCard</div>
-}
+import { backendUrl } from "../Api/api.js";
+const BoatCard = ({ boat }) => {
+  return <section className="flex flex-col items-center border-2 rounded-xl shadow-md ">
+    {/* <img src={`${backendUrl}/${boat.imgUrl}`} alt={boat.name} /> */}
+    <h2>Name: {boat.name}</h2>
+    <p>Type: {boat.boatsType}</p>
+    <p>Baujahr: {boat.baujahr}</p>
+    <p>SN: {boat.seriennummer}</p>
+    <p>Material: {boat.material}</p>
 
-export default BoatCard
+  </section>;
+};
+
+export default BoatCard;
--- frontend/src/pages/Boats.jsx
+++ frontend/src/pages/Boats.jsx
@@ -1,5 +1,26 @@
-const Boats = () => {
-  return <div>Boats</div>
-}
+import { useContext } from "react";
+import BoatCard from "../components/BoatCard";
+import { AllBoats } from "../Context/Context";
 
-export default Boats
+const Boats = () => {
+  const { allBoats } = useContext(AllBoats);
+
+  return <section>
+    <div className="flex gap-2 justify-center">
+      <button>All Boats</button>
+      <button>All Reservations</button>
+      <button>All Available</button>
+    </div>
+    <article className="grid sm:grid-cols-3 grid-cols-1 gap-4 px-5 ">
+      {allBoats.map((boat) => (
+        <div key={boat._id} >
+          <BoatCard boat={boat} />
+        </div>
+      ))}
+
+    </article>
+    <button className="border rounded-xl p-2 shadow-md cursor-pointer">Add New Boat</button>
+  </section>;
+};
+
+export default Boats;
