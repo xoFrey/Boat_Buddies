b39e8c1e0a0963b216edbbcb201ef2126da5cbf5
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-04-30 08:40:49 +0200

    Add Basic Form on Details Page for Reservation

--- frontend/src/components/ResForm.jsx
+++ frontend/src/components/ResForm.jsx
@@ -1,5 +1,25 @@
+import { useParams } from "react-router-dom"
+
 const ResForm = () => {
-  return <div>ResForm</div>
+  const { boatsId } = useParams()
+
+  return (
+    <div className="px-8">
+      <h2 className="font-bold">Reserve a nice and juicy boat</h2>
+      <form className="flex flex-col gap-2">
+        <input type="text" defaultValue={boatsId} />
+        <input type="text" name="username" id="username" />
+        <input type="text" name="phone" id="phone" />
+        <input type="email" name="email" id="email" />
+      </form>
+    </div>
+  )
 }
 
 export default ResForm
+
+// name: { type: String, required: true, trim: true },
+// phone: { type: String, required: true, trim: true },
+// email: { type: String, required: true, trim: true },
+// startDate: { type: Date, required: true, default: Date.now(), trim: true },
+// endDate: { type: Date, required: true, trim: true }, // # Date.now() Plus
--- frontend/src/pages/Details.jsx
+++ frontend/src/pages/Details.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { backendUrl } from "../Api/api.js"
+import ResForm from "../components/ResForm.jsx"
 
 const Details = () => {
   const [boatsData, setBoatsData] = useState([])
@@ -14,29 +15,34 @@ const Details = () => {
   }, [])
 
   return (
-    <section>
-      <div className="mb-8">
-        <h1>Boats Details Page</h1>
-        <h2>{boatsData.name}</h2>
-        <h2>SN: {boatsData.seriennummer}</h2>
-        <h2>Type: {boatsData.boatsType}</h2>
-        <h2>Material: {boatsData.material}</h2>
-      </div>
+    <>
+      <section>
+        <div className="mb-8">
+          <h1>Boats Details Page</h1>
+          <h2>{boatsData.name}</h2>
+          <h2>SN: {boatsData.seriennummer}</h2>
+          <h2>Type: {boatsData.boatsType}</h2>
+          <h2>Material: {boatsData.material}</h2>
+        </div>
 
-      <h2 className="font-bold">Reservations</h2>
-      {boatsData.reservations &&
-        boatsData.reservations.map((item) => {
-          return (
-            <div key={item._id}>
-              <p>{item.name}</p>
-              <p>{item.phone}</p>
-              <p>{item.email}</p>
-              <p>{item.startDate}</p>
-              <p>{item.endDate}</p>
-            </div>
-          )
-        })}
-    </section>
+        <h2 className="font-bold">Reservations</h2>
+        {boatsData.reservations &&
+          boatsData.reservations.map((item) => {
+            return (
+              <div className="mb-8" key={item._id}>
+                <p>{item.name}</p>
+                <p>{item.phone}</p>
+                <p>{item.email}</p>
+                <p>{item.startDate}</p>
+                <p>{item.endDate}</p>
+              </div>
+            )
+          })}
+      </section>
+      <section>
+        <ResForm />
+      </section>
+    </>
   )
 }
 
