b31fef1215fb62ee0bda9e2427f1415ab2e34cd3
xofrey <xizelacar@gmail.com>
2024-04-30 10:13:21 +0200

    delete functions for boats and reservations

--- frontend/src/pages/Details.jsx
+++ frontend/src/pages/Details.jsx
@@ -1,12 +1,17 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import { backendUrl } from "../Api/api.js";
 import ResForm from "../components/ResForm.jsx";
+import { TiDeleteOutline } from "react-icons/ti";
+import { AllBoats } from "../Context/Context.jsx";
+
 
 const Details = () => {
+  const { allBoats, setAllBoats } = useContext(AllBoats);
   const [boatsData, setBoatsData] = useState([]);
   const { boatsId } = useParams();
   const [newRes, setNewRes] = useState();
+
 
   useEffect(() => {
     fetch(`${backendUrl}/api/v1/boats/${boatsId}`)
@@ -15,16 +20,37 @@ const Details = () => {
       .catch((err) => console.log(err));
   }, [newRes]);
 
+  const deleteBoat = () => {
+    fetch(`${backendUrl}/api/v1/boats/${boatsId}`, { method: "DELETE" })
+      .then((res) => res.json())
+      .then((data) => setAllBoats(allBoats.filter((item) => data._id !== item._id)))
+      .then(() => fetch(`${backendUrl}/api/v1/reservations/${boatsId}`, { method: "DELETE" }))
+      .catch((err) => console.log(err));
+
+  };
+
+
+  const deleteReservation = () => {
+    fetch(`${backendUrl}/api/v1/reservations/${boatsId}`, { method: "DELETE" })
+      .then((res) => res.json())
+      .then((data) => setNewRes(data))
+      .catch((err) => console.log(err));
+
+  };
+
 
   return (
     <>
       <section>
-        <div className="mb-8">
+        <div className="mb-8 flex flex-col items-start">
           <h1>Boats Details Page</h1>
           <h2>{boatsData.name}</h2>
           <h2>SN: {boatsData.seriennummer}</h2>
           <h2>Type: {boatsData.boatsType}</h2>
           <h2>Material: {boatsData.material}</h2>
+          <Link to="/boats" className="inline">
+            <TiDeleteOutline onClick={deleteBoat} className="cursor-pointer text-red-500" size={30} />
+          </Link>
         </div>
 
         <h2 className="font-bold">Reservations</h2>
@@ -37,6 +63,7 @@ const Details = () => {
                 <p>{item.email}</p>
                 <p>{item.startDate}</p>
                 <p>{item.endDate}</p>
+                <TiDeleteOutline onClick={deleteReservation} className="cursor-pointer text-red-500" size={30} />
               </div>
             );
           })}
